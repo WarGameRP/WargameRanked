6994148f6ddebf409d18a1e16714627e06b26f6a
FreeProject089 <enrique089alarcon@gmail.com>
2026-04-21 01:21:40 +0200

    add menu

---
+++ discord_servers.js
@@ -0,0 +1,16 @@
+// Liste des serveurs Discord
+// Ajoutez simplement un nouvel objet dans le tableau pour ajouter un serveur
+window.DISCORD_SERVERS = [
+    {
+        name: "WarGameRanked",
+        description: "Serveur principal de la communauté",
+        url: "https://discord.gg/s3tyvhMQwV",
+        icon: "WR"
+    },
+    {
+        name: "Recherche Joueur",
+        description: "Salon recherche de joueurs",
+        url: "https://discord.gg/7bRbdTP8tt",
+        icon: "RJ"
+    }
+];
